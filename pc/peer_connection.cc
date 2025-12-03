b8e3e5d876b0764bedfbbb6565bb53030f0f970b
Youenn Fablet <youenn@apple.com>
2025-12-03 11:05:27 +0100

    pc fixes

--- pc/peer_connection.cc
+++ pc/peer_connection.cc
@@ -711,9 +711,15 @@ JsepTransportController* PeerConnection::InitializeTransportController_n(
   config.ice_transport_factory = ice_transport_factory_.get();
   config.on_dtls_handshake_error_ =
       [weak_ptr = weak_factory_.GetWeakPtr()](SSLHandshakeError s) {
+#if defined(WEBRTC_WEBKIT_BUILD)
+        RTC_HISTOGRAM_ENUMERATION(
+            "WebRTC.PeerConnection.DtlsHandshakeError", static_cast<int>(s),
+            static_cast<int>(SSLHandshakeError::MAX_VALUE));
+#else
         if (weak_ptr) {
           weak_ptr->OnTransportControllerDtlsHandshakeError(s);
         }
+#endif
       };
 
   transport_controller_.reset(new JsepTransportController(
