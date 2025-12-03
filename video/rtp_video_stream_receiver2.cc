e747660377aed30a3960c12302cd814add2bfa4d
Youenn Fablet <youenn@apple.com>
2025-12-03 11:06:20 +0100

    video/rtp_video_stream_receiver2 fixes

--- video/rtp_video_stream_receiver2.cc
+++ video/rtp_video_stream_receiver2.cc
@@ -929,6 +929,32 @@ void RtpVideoStreamReceiver2::OnAssembledFrame(
   RTC_DCHECK_RUN_ON(&packet_sequence_checker_);
   RTC_DCHECK(frame);
 
+#if defined(WEBRTC_WEBKIT_BUILD)
+    static const unsigned MaxFrameDelayCount = 3;
+    static const unsigned TimeStampQueueDurationInMs = 2000;
+    static const unsigned OneSecondInMs = 1000;
+    // FIXME: Consider using MovingAverage instead.
+    ++frameCount_;
+
+    auto frameTime = env_.clock().TimeInMilliseconds();
+    auto lastFrameTime = observedFrameTimeStamps_.empty() ? frameTime : observedFrameTimeStamps_.back();
+
+    if (observedFrameRate_) {
+      auto delay = frameTime - lastFrameTime;
+      if (delay > (MaxFrameDelayCount * OneSecondInMs / observedFrameRate_)) {
+        RTC_LOG(LS_INFO) << "RtpVideoStreamReceiver::OnAssembledFrame at " << frameTime << ", previous frame was at " << lastFrameTime << ", observed frame rate is " << observedFrameRate_ << ", delay since last frame is " << (frameTime - lastFrameTime) <<  " ms, frame count is " << frameCount_ << ", size is " << frame->EncodedImage().size() << ", frame reception duration is " << (frame->video_timing().receive_finish_ms - frame->video_timing().receive_start_ms) << " ms, " << " observed samples count is " << observedFrameTimeStamps_.size() << ", observed samples duration is " << (observedFrameTimeStamps_.back() - observedFrameTimeStamps_.front());
+      }
+    }
+
+    observedFrameTimeStamps_.push_back(frameTime);
+    while (observedFrameTimeStamps_.front() <= (frameTime - TimeStampQueueDurationInMs))
+        observedFrameTimeStamps_.pop_front();
+
+    auto queueDuration = frameTime - observedFrameTimeStamps_.front();
+    if (queueDuration > OneSecondInMs)
+        observedFrameRate_ = observedFrameTimeStamps_.size() * OneSecondInMs / queueDuration;
+#endif
+
   const std::optional<RTPVideoHeader::GenericDescriptorInfo>& descriptor =
       frame->GetRtpVideoHeader().generic;
 
