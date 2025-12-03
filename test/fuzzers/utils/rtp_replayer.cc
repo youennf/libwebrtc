b93c5e3643542de53f9892d66f00b7a45d413807
Youenn Fablet <youenn@apple.com>
2025-12-03 11:06:02 +0100

    test fixes

--- test/fuzzers/utils/rtp_replayer.cc
+++ test/fuzzers/utils/rtp_replayer.cc
@@ -21,7 +21,9 @@
 #include "media/engine/internal_decoder_factory.h"
 #include "modules/rtp_rtcp/source/rtp_packet.h"
 #include "modules/rtp_rtcp/source/rtp_packet_received.h"
+#if !WEBRTC_WEBKIT_BUILD
 #include "rtc_base/strings/json.h"
+#endif
 #include "system_wrappers/include/clock.h"
 #include "test/call_config_utils.h"
 #include "test/encoder_settings.h"
@@ -32,6 +34,7 @@
 namespace webrtc {
 namespace test {
 
+#if !WEBRTC_WEBKIT_BUILD
 void RtpReplayer::Replay(const std::string& replay_config_filepath,
                          const uint8_t* rtp_dump_data,
                          size_t rtp_dump_size) {
@@ -41,6 +44,7 @@ void RtpReplayer::Replay(const std::string& replay_config_filepath,
   return Replay(std::move(stream_state), std::move(receive_stream_configs),
                 rtp_dump_data, rtp_dump_size);
 }
+#endif
 
 void RtpReplayer::Replay(
     std::unique_ptr<StreamState> stream_state,
@@ -89,6 +93,7 @@ void RtpReplayer::Replay(
   }
 }
 
+#if !WEBRTC_WEBKIT_BUILD
 std::vector<VideoReceiveStreamInterface::Config>
 RtpReplayer::ReadConfigFromFile(const std::string& replay_config,
                                 Transport* transport) {
@@ -113,6 +118,7 @@ RtpReplayer::ReadConfigFromFile(const std::string& replay_config,
   }
   return receive_stream_configs;
 }
+#endif
 
 void RtpReplayer::SetupVideoStreams(
     std::vector<VideoReceiveStreamInterface::Config>* receive_stream_configs,
@@ -181,8 +187,15 @@ void RtpReplayer::ReplayPackets(
       RTC_LOG(LS_ERROR) << "Packet error, corrupt packets or incorrect setup?";
       break;
     }
+#ifdef WEBRTC_WEBKIT_BUILD
+    // Set the clock rate if zero - always 90K for video
+    if (received_packet.payload_type_frequency() == 0) {
+        received_packet.set_payload_type_frequency(kVideoPayloadTypeFrequency);
+    }
+#else
     // Set the clock rate - always 90K for video
     received_packet.set_payload_type_frequency(kVideoPayloadTypeFrequency);
+#endif
 
     call->Receiver()->DeliverRtpPacket(
         MediaType::VIDEO, std::move(received_packet),
