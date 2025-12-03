0b600e02cfa89141fd81a3403ed1d1596a7e848f
Youenn Fablet <youenn@apple.com>
2025-12-03 11:04:42 +0100

    modules/video_coding fixes

--- modules/video_coding/codecs/h264/h264_encoder_impl.h
+++ modules/video_coding/codecs/h264/h264_encoder_impl.h
@@ -38,7 +38,15 @@
 #include "modules/video_coding/codecs/h264/include/h264.h"
 #include "modules/video_coding/codecs/h264/include/h264_globals.h"
 #include "modules/video_coding/svc/scalable_video_controller.h"
-#include "third_party/openh264/src/codec/api/wels/codec_app_def.h"
+#include "modules/video_coding/utility/quality_scaler.h"
+
+#ifdef WEBRTC_WEBKIT_BUILD
+#if defined(WEBKIT_LIBWEBRTC_OPENH264_ENCODER) && WEBKIT_LIBWEBRTC_OPENH264_ENCODER
+#include "wels/codec_app_def.h"
+#else
+#include "third_party/openh264/src/codec/api/svc/codec_app_def.h"
+#endif
+#endif
 
 #if defined(WEBRTC_WIN) && !defined(__clang__)
 #error "See: bugs.webrtc.org/9213#c13."
